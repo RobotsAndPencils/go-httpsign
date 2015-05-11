5e385c154f040264af057c164c43462f048c4524
Christopher Chew <chris@Christophers-MacBook-Pro.local>
2015-05-11 14:02:54 -0700

    Add a log hook for troubleshooting Verify() in the wild.

--- httpsign_test.go
+++ httpsign_test.go
@@ -167,6 +167,56 @@ func TestVerifyWhenDisabled(t *testing.T) {
 	assert.Equal(http.StatusOK, resp.StatusCode)
 }
 
+func TestVerifyLogs(t *testing.T) {
+	assert := assert.New(t)
+	value := randomString(25)
+	key := []byte(randomString(100))
+	hs := New(key)
+	var lastLogMsg string
+	hs.LogHook = func(msg string) {
+		lastLogMsg = msg
+	}
+
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	v := func(w http.ResponseWriter, r *http.Request) string {
+		return value
+	}
+
+	ts := httptest.NewServer(hs.Verify(h, v))
+	defer ts.Close()
+
+	// make request with no header
+	req, err := http.NewRequest("GET", ts.URL, nil)
+	assert.Nil(err)
+	resp, err := http.DefaultClient.Do(req)
+	assert.Nil(err)
+	defer resp.Body.Close()
+	assert.Equal(http.StatusBadRequest, resp.StatusCode)
+
+	// make request with an invalid header
+	header := hs.GenerateHeaderValue("some random value")
+	req, err = http.NewRequest("GET", ts.URL, nil)
+	assert.Nil(err)
+	req.Header.Set(hs.HeaderName, header)
+	resp, err = http.DefaultClient.Do(req)
+	assert.Nil(err)
+	defer resp.Body.Close()
+	assert.Contains(lastLogMsg, "Signature mismatch")
+
+	// make request with a stale timestamp
+	header = formHeader("does not matter", 12345) // Long time ago in a galaxy far away...
+	req, err = http.NewRequest("GET", ts.URL, nil)
+	assert.Nil(err)
+	req.Header.Set(hs.HeaderName, header)
+	resp, err = http.DefaultClient.Do(req)
+	assert.Nil(err)
+	defer resp.Body.Close()
+	assert.Contains(lastLogMsg, "Stale timestamp 12345")
+}
+
 func randomString(size int) string {
 	alphanum := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	var bytes = make([]byte, size)
